ed81f29741324c9bda26e15bbfd64c4789962a42
Prajwalkrishnappa04 <prajwalkrishnappa04@gmail.com>
2025-08-25 17:12:40 +0530

    Added code for line clearance

--- merai_newage/public/js/job_card.js
+++ merai_newage/public/js/job_card.js
@@ -48,5 +48,26 @@ frappe.ui.form.on("Job Card", {
                     });
                 }
             });
+
+
+            if (frm.doc.operation) {
+            frappe.db.get_doc("Operation", frm.doc.operation).then(op => {
+                if (op.custom_line_clearance_template) {
+                    let template_name = op.custom_line_clearance_template;
+
+                    frappe.db.get_doc("Line Clearance Template", template_name).then(template => {
+                        frm.clear_table("custom_line_clearance_checklist");
+
+                        (template.line_clearence_checkpoints || []).forEach(row => {
+                            let child = frm.add_child("custom_line_clearance_checklist");
+                            child.line_clearance_criteria = row.line_clearance_criteria;
+                            child.yesno = row.yesno;
+                        });
+
+                        frm.refresh_field("custom_line_clearance_checklist");
+                    });
+                }
+            });
+        }
     }
 });
