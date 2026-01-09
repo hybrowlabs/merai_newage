6b4a2e93ebaf6b8e7c79c4ef533f579d4efbcfa1
Prajwalkrishnappa04 <prajwalkrishnappa04@gmail.com>
2026-01-09 16:13:35 +0530

    added modified BRC

--- merai_newage/merai_newage/doctype/batch_release_certificate/batch_release_certificate.py
+++ merai_newage/merai_newage/doctype/batch_release_certificate/batch_release_certificate.py
@@ -47,6 +47,22 @@ def create_brc(work_order):
     new_brc.robot_serial_no = frappe.db.get_value("Robot Tracker",{"work_order":work_order},"name")
 
     item_doc = frappe.get_doc("Item", item_code)
+    item_group = frappe.db.get_value("Item",item_code,"item_group")
+    template_name = frappe.db.get_value(
+            "Item Group",
+            item_group,
+            "custom_installtion_procedure__verification_template"
+        )
+    template_doc = frappe.get_doc(
+            "Installation Procedure And Verification Template",
+            template_name
+        )
+    print("template_doc-----------",template_doc)
+    for row in template_doc.brc_check_deatils:
+            
+            new_brc.append("batch_release_certificate_details", {
+                "test_description": row.check_name
+            })
 
     for row in item_doc.custom_dispatch_checklist_details:
         new_brc.append(
@@ -63,25 +79,37 @@ def create_brc(work_order):
     return new_brc.name
 
 
-
 @frappe.whitelist()
 def fetch_brc_details(work_order):
-    # Get Work Order document
-    wo_doc = frappe.get_doc("Work Order", work_order)
-    
-    # Get Item code from Work Order
-    item_code = wo_doc.production_item
-    
-    # Get Item document to fetch child table data
+
+    wo_doc = frappe.get_doc("Work Order", work_order)    
+    item_code = wo_doc.production_item 
+
     item_doc = frappe.get_doc("Item", item_code)
-    
-    # Prepare response data
+    item_group = frappe.db.get_value("Item", item_code, "item_group")
+
+    template_name = frappe.db.get_value(
+        "Item Group",
+        item_group,
+        "custom_installtion_procedure__verification_template"
+    )
+
     response = {
-       
-        "child_items": []
+        "child_items": [],          
+        "verification_items": []    
     }
-    
-    # Fetch child table data from Item Master
+
+    if template_name:
+        template_doc = frappe.get_doc(
+            "Installation Procedure And Verification Template",
+            template_name
+        )
+
+        for row in template_doc.brc_check_deatils:
+            response["verification_items"].append({
+                "test_description": row.check_name
+            })
+
     if hasattr(item_doc, 'custom_dispatch_checklist_details'):
         for row in item_doc.custom_dispatch_checklist_details:
             response["child_items"].append({
@@ -89,5 +117,25 @@ def fetch_brc_details(work_order):
                 "std_qty": row.qty,
                 "description": row.product_description
             })
-    
+
     return response
+
+
+
+# @frappe.whitelist()
+# def create_dispatch(doc):
+#     doc = frappe.parse_json(doc)
+
+#     new_dispatch = frappe.new_doc("Dispatch")
+#     new_dispatch.robot_classification = doc.get("robot_classifcation")
+#     new_dispatch.item_code = doc.get("item_code")
+#     new_dispatch.dispatch_no = doc.get("name")
+#     new_dispatch.hospital_name = doc.get("hospital_name")
+#     new_dispatch.work_order = doc.get("work_order")
+#     new_dispatch.hospital_address = frappe.db.get_value("Account Master",doc.hospital_name,"address")
+#     new_dispatch.city = frappe.db.get_value("Account Master",doc.hospital_name,"city")
+#     new_dispatch.state = frappe.db.get_value("Account Master",doc.hospital_name,"state")
+
+#     new_assign_installation.insert(ignore_permissions=True)  # Save the doc
+
+#     return new_assign_installation.name
