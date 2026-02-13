ed8c06518701cb4e84a0335dbae3331d19bedbd3
nitesh2910 <goadnitesh4813@gmail.com>
2026-02-13 08:49:42 +0000

    Rename function of dimensional calculation

--- merai_newage/public/js/dimension_calculation.js
+++ merai_newage/public/js/dimension_calculation.js
@@ -1,57 +1,82 @@
 function calculation_box_and_gross_weight(frm, cdt, cdn) {
-  const row = locals[cdt][cdn];
-  if (!row) return;
 
-  const box = flt(row.box || 0);
-  const weight = flt(row.weight || 0);
+    const row = frappe.get_doc(cdt, cdn);
+    if (!row) return;
 
-  const gross = flt(box * weight, 3);
-  frappe.model.set_value(cdt, cdn, "gross_weight", gross);
+    const box = flt(row.box);
+    const weight = flt(row.weight);
+
+    const gross_weight = flt(box * weight, 3);
+
+    frappe.model.set_value(cdt, cdn, "gross_weight", gross_weight);
 }
 
-function dimension_calculation(frm, cdt, cdn) {
-  const row = locals[cdt][cdn];
-  if (!row) return;
+/**
+ * Calculate Volume Metric Weight for Row
+ * And Update Parent Chargeable Weight
+ */
+function calculate_dimension_row(frm, cdt, cdn) {
 
-  const length = flt(row.length || 0);
-  const width = flt(row.width || 0);
-  const height = flt(row.height || 0);
-  const gross_weight = flt(row.gross_weight || 0);
-  const type_wise_value = flt(frm.doc.type_wise_value || 1);
+    const row = frappe.get_doc(cdt, cdn);
+    if (!row) return;
 
-  const volume =
-    (length * width * height * gross_weight) / type_wise_value;
+    const length = flt(row.length);
+    const width = flt(row.width);
+    const height = flt(row.height);
+    const gross_weight = flt(row.gross_weight);
+    const type_wise_value = flt(frm.doc.type_wise_value) || 1;
 
-  frappe.model.set_value(cdt,cdn,"custom_volume_metric_weight_cm",flt(volume, 3));
+    // Row Volume
+    const volume =
+        (length * width * height * gross_weight) / type_wise_value;
 
-  // Parent total
-  let total = 0;
-  (frm.doc.dimension_calculation || []).forEach((d) => {
-    total += flt(d.custom_volume_metric_weight_cm || 0);
-  });
+    frappe.model.set_value(
+        cdt,
+        cdn,
+        "custom_volume_metric_weight_cm",
+        flt(volume, 3)
+    );
 
-  frm.set_value("chargeable_weight", flt(total, 3));
+    // Calculate Parent Total
+    let total = 0;
+
+    (frm.doc.dimension_calculation || []).forEach(d => {
+        total += flt(d.custom_volume_metric_weight_cm);
+    });
+
+    frm.set_value("chargeable_weight", flt(total, 3));
 }
 
+
+/**
+ * Child Table Events
+ */
 frappe.ui.form.on("Dimension Calculation", {
-  length(frm, cdt, cdn) {
-    dimension_calculation(frm, cdt, cdn);
-  },
-  width(frm, cdt, cdn) {
-    dimension_calculation(frm, cdt, cdn);
-  },
-  height(frm, cdt, cdn) {
-    dimension_calculation(frm, cdt, cdn);
-  },
-  gross_weight(frm, cdt, cdn) {
-    dimension_calculation(frm, cdt, cdn);
-  },
-  box(frm, cdt, cdn) {
-    calculation_box_and_gross_weight(frm, cdt, cdn);
-    dimension_calculation(frm, cdt, cdn);
-  },
-  weight(frm, cdt, cdn) {
-    calculation_box_and_gross_weight(frm, cdt, cdn);
-    dimension_calculation(frm, cdt, cdn);
-  }
+
+    length(frm, cdt, cdn) {
+        calculate_dimension_row(frm, cdt, cdn);
+    },
+
+    width(frm, cdt, cdn) {
+        calculate_dimension_row(frm, cdt, cdn);
+    },
+
+    height(frm, cdt, cdn) {
+        calculate_dimension_row(frm, cdt, cdn);
+    },
+
+    gross_weight(frm, cdt, cdn) {
+        calculate_dimension_row(frm, cdt, cdn);
+    },
+
+    box(frm, cdt, cdn) {
+        calculation_box_and_gross_weight(frm, cdt, cdn);
+        calculate_dimension_row(frm, cdt, cdn);
+    },
+
+    weight(frm, cdt, cdn) {
+        calculation_box_and_gross_weight(frm, cdt, cdn);
+        calculate_dimension_row(frm, cdt, cdn);
+    }
+
 });
