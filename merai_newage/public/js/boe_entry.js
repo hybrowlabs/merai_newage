770e82c57047b8e9a1c3b7eed6c3b3ff4f7df297
nitesh2910 <goadnitesh4813@gmail.com>
2026-03-07 04:57:27 +0000

    Added workflow attachment Till Eway-bill doctype and some chnages in json

--- merai_newage/public/js/boe_entry.js
+++ merai_newage/public/js/boe_entry.js
@@ -1,4 +1,4 @@
-frappe.ui.form.on('Pre Alert', {
+frappe.ui.form.on('BOE Entry', {
 
     refresh: function (frm) {
 		if (!frm._attachment_sync_bound && frm.attachments) {
@@ -24,18 +24,4 @@ frappe.ui.form.on('Pre Alert', {
         }
     }
 
-});
-
-frappe.ui.form.on('Pre-Alert Item Details', {
-    igcr: function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
-
-        if (row.igcr) {
-            frappe.model.set_value(cdt, cdn, 'category', '9');
-            frm.fields_dict.item_details.grid.toggle_enable('category', false);
-        } else {
-            frappe.model.set_value(cdt, cdn, 'category', null);
-            frm.fields_dict.item_details.grid.toggle_enable('category', true);
-        }
-    }
 });
