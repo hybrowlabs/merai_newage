cff9aea9439c3687476afce3d63d0bae1f042367
nitesh2910 <goadnitesh4813@gmail.com>
2026-04-03 09:08:06 +0000

    Created Rodtep indivual level apply

--- merai_newage/merai_newage/doctype/po_condition_change/po_condition_change.js
+++ merai_newage/merai_newage/doctype/po_condition_change/po_condition_change.js
@@ -1,64 +1,3 @@
-// frappe.ui.form.on("PO Condition Change", {
-//     refresh: function (frm) {
-
-//         if (frm.doc.docstatus == 1) {
-
-//             frm.add_custom_button("e-Waybill", function () {
-
-//                 let items = [];
-//                 let total_amount = 0;
-
-//                 // 🔹 Child table se items uthao
-//                 if (frm.doc.po_condition_change_items) {
-//                     frm.doc.po_condition_change_items.forEach(item => {
-
-//                         items.push({
-//                             purchase_order: item.purchasing_doc,
-//                             item_code: item.material,
-//                             item_name: item.material,
-//                             qty: item.order_quantity,
-//                             po_qty: item.order_quantity,
-//                             total_inr_value: item.amount,
-//                             rate: item.amount / (item.order_quantity || 1),
-//                             amount: item.amount,
-//                             currency: item.currency || frm.doc.currency,
-//                             rate_inr: item.amount / (item.order_quantity || 1)
-//                         });
-
-//                         total_amount += item.amount;
-//                     });
-//                 }
-
-//                 // 🔹 E-Waybill create karo
-//                 frappe.call({
-//                     method: "frappe.client.insert",
-//                     args: {
-//                         doc: {
-//                             doctype: "E-way Bill",
-
-//                             //SAME LOGIC (important)
-//                             select_doctype: frm.doctype,
-//                             doctype_id: frm.doc.name,
-
-//                             pre_alert_check_list: frm.doc.pre_alert_req,
-//                             supplier: frm.doc.vendor,
-
-//                             items: items
-//                         }
-//                     },
-//                     callback: function (r) {
-//                         if (r.message) {
-//                             frappe.set_route("Form", "E-way Bill", r.message.name);
-//                         }
-//                     }
-//                 });
-
-//             }, __("Create"));
-//         }
-//     }
-// });
-
-
 frappe.ui.form.on("PO Condition Change", {
     refresh: function (frm) {
 
@@ -165,11 +104,18 @@ frappe.ui.form.on("PO Condition Change", {
             }, __("Get Detail"));
         }
 
-
-        // 🔹 EXISTING E-WAYBILL BUTTON (UNCHANGED)
         if (frm.doc.docstatus == 1) {
 
             frm.add_custom_button("e-Waybill", function () {
+
+                // STEP 1: Check if already linked
+                if (frm.doc.e_way_bill) {
+                    frappe.msgprint("E-Way Bill already created");
+
+                    // direct open existing EWB
+                    frappe.set_route("Form", "E-way Bill", frm.doc.e_way_bill);
+                    return;
+                }
 
                 let items = [];
                 let total_amount = 0;
@@ -194,6 +140,7 @@ frappe.ui.form.on("PO Condition Change", {
                     });
                 }
 
+                // STEP 2: Create new EWB
                 frappe.call({
                     method: "frappe.client.insert",
                     args: {
@@ -208,6 +155,10 @@ frappe.ui.form.on("PO Condition Change", {
                     },
                     callback: function (r) {
                         if (r.message) {
+
+                            // Save link back in PO Condition Change
+                            frappe.db.set_value("PO Condition Change", frm.doc.name, "e_way_bill", r.message.name);
+
                             frappe.set_route("Form", "E-way Bill", r.message.name);
                         }
                     }
@@ -216,7 +167,7 @@ frappe.ui.form.on("PO Condition Change", {
             }, __("Create"));
         }
     },
-    // 🔹 Attachment Sync hooks
+    // Attachment Sync hooks
     after_save: function(frm) {
         if (merai?.sync_workflow_attachment_table) {
             merai.sync_workflow_attachment_table(frm);
